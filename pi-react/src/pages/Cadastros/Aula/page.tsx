b2a198350df9056bdfb29accab1fb2ef28e2dfe3
Luana Rodrigues <chavedl578@gmail.com>
2025-10-25 19:48:38 -0300

    WIP: Pagina de cadastro de aula

--- pi-react/src/pages/Cadastros/Aula/page.tsx
+++ pi-react/src/pages/Cadastros/Aula/page.tsx
@@ -1,20 +1,24 @@
 import { useNavigate } from "react-router-dom";
 import Botao from "./../../../components/Botao/Botao"
+import Input from "./../../../components/Input/Input"
 
 export default function Cadastro_Aula(){
 
   const navigate = useNavigate();
 
     return(
-        <main>
-            <header>
-                <div>
+        <main className="w-full h-full flex flex-col px-[15%]">
+            <header className="w-full h-full flex flex-row">
+                <div className="w-full text-[2rem] font-semibold">
                     <h1>Cadastrar aula</h1>
                 </div>
                 <div className="w-full">
                     <Botao texto="Voltar a página anterior" type="button" onClick={() => navigate(-1)}/>
                 </div>
             </header>
+            <form action="">
+                
+            </form>
         </main>
     )
 }
